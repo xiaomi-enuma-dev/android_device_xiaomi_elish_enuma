2cd7bb0764baa3a0c8d2df029565f42511347d47
Fauzan Amir Al Ghiffary <56854433+alghiffaryfa19@users.noreply.github.com>
2025-09-29 15:56:08 +0700

    change mk file

--- lineage_enuma.mk
+++ lineage_enuma.mk
@@ -6,10 +6,10 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
+$(call inherit-product, vendor/lineage/config/common_full_tablet.mk)
 
 PRODUCT_CHARACTERISTICS := tablet
 
